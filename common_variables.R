a564fd157ea3414b232f189d17cebf975046b07a
lastoel <l.a.stoel@students.uu.nl>
2022-01-23 17:39:30 +0100

    Updated CV

---
+++ common_variables.R
@@ -0,0 +1,24 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "I've followed a somewhat unconventional path to where I am now: a second-year research masters student in Methodology and Statistics. My undergraduate professor in statistics inspired me to persue a quantitative degree, but I also got the opportunity from my internship at the Dutch Ministry of Education to apply for a job as policy officer at the Dutch Research Council. This allowed me to further develope my love for education and my knowledge of our education system. 
+
+I started my masters degree in 2020, moving to part-time employment at NRO. Now, my work experience and field of studies are reunited in the field of Educational Measurement. 
+"
+
+
+# Language skills for skill bars visualization
+Progskills <- tribble(
+  ~skill,               ~level,
+  "R",                  4.5,
+  "Python",             3
+)
+
+#Language skills
+Langskills <- tribble(
+  ~skill,      ~level,
+  "Dutch",     5,
+  "English",   5,
+  "German",    4,
+  "French",    3
+)
